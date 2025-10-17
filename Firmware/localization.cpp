21c724423357106690fe2bfe50045928e8b2e043
Youssra Farghaly <youssramhfarghaly@gmail.com>
2025-10-17 18:23:22 +0300

    added firmware folder and localization file

---
+++ Firmware/localization.cpp
@@ -0,0 +1,65 @@
+#include <math.h>
+#include <string>
+using namespace std;
+
+class localization
+{
+private:
+    const float wheel_radius;
+    const float wheel_base;
+    const int right_ticks_per_rev;
+    const int left_ticks_per_rev;
+
+    float right_distance_per_tick;
+    float left_distance_per_tick;
+
+    float x = 0.0;
+    float y = 0.0;
+    float theta = 0.0;
+
+    long prev_left_ticks = 0;
+    long prev_right_ticks = 0;
+    float prev_imu_heading = 0.0;
+
+public:
+    localization(float wheelRadius, float wheelBase, int rightTicksPerRev, int leftTicksPerRev)
+        : wheel_radius(wheelRadius),
+          wheel_base(wheelBase),
+          right_ticks_per_rev(rightTicksPerRev),
+          left_ticks_per_rev(leftTicksPerRev)
+    {
+        right_distance_per_tick = 2 * M_PI * wheel_radius / right_ticks_per_rev;
+        left_distance_per_tick = 2 * M_PI * wheel_radius / left_ticks_per_rev;
+    }
+
+    string update(long left_ticks, long right_ticks, float imu_heading)
+    {
+        float left_dist = left_distance_per_tick * (left_ticks - prev_left_ticks);
+        float right_dist = right_distance_per_tick * (right_ticks - prev_right_ticks);
+        float center_dist = (left_dist + right_dist) / 2.0;
+        // float delta_theta = (right_dist - left_dist) / 2.0;
+
+        // apply kalman filter and get esmited_heading and use it instead
+        // x += center_dist * sin(estimated_heading);
+        // y += center_dist * cos(estimated_heading);
+
+        x += center_dist * sin(imu_heading);
+        y += center_dist * cos(imu_heading);
+
+        // x += center_dist * sin(delta_theta / 2.0);
+        // y += center_dist * cos(delta_theta / 2.0);
+        // theta += delta_theta;
+        // while(theta > M_PI) theta -= 2*M_PI;
+        // while(theta < -M_PI)theta += 2*M_PI;
+
+        prev_left_ticks = left_ticks;
+        prev_right_ticks = right_ticks;
+        prev_imu_heading = imu_heading;
+        return "X=" + to_string(x) + ", Y=" + to_string(y) + ", angle=" + to_string(imu_heading);
+    }
+
+    float getX() const { return x; }
+    float getY() const { return y; }
+    float getHeading() const { return prev_imu_heading; }
+    // float getTheta() const { return theta; }
+};
