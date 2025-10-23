a5c59e14a354e8ad59541213b3c61288655fc701
yaya <yaya04183@gmail.com>
2025-10-23 15:53:52 +0300

    Add files via upload

---
+++ ros2_ws/src/RobotGui_pkg/RobotGui_pkg/core/comm/Publisher_speeds.py
@@ -0,0 +1,36 @@
+import paho.mqtt.client as mqtt 
+import time 
+
+broker = "10.96.185.112"  ## use ip address of wifi or use the ip address of esp 
+port = 1883 
+Topic = "AUR/controls"
+
+## we need the node "controller node" 
+
+Current_Motor_Right_Speed = 0.0
+Current_Motor_left_Speed = 0.0
+
+def Publisher_Motor_Speeds(client):
+    Right_Speed_Motor = Current_Motor_Right_Speed
+    Left_Speed_Motor = Current_Motor_left_Speed
+    message = f"Right_Speed_Motor:{Right_Speed_Motor} , Left_Speed_Motor:{Left_Speed_Motor}"
+    print(f"Speeds Of Motors = {message}")
+    client.publish(Topic,message)  ## Topic First 
+
+def Start_Publisher():
+    client = mqtt.Client()
+    try :
+        client.connect(broker,port,60)
+        connection_successful = True
+    except Exception as e :
+        connection_successful = False 
+    
+    if connection_successful :
+        while True :
+          client.loop_start()
+          Publisher_Motor_Speeds(client)
+          time.sleep(1)
+    else : 
+        print("No Connection , An Error Has Occured")   
+if __name__=="__main__":
+    Start_Publisher()
---
+++ ros2_ws/src/RobotGui_pkg/RobotGui_pkg/core/comm/Subscriber.py
@@ -0,0 +1,42 @@
+import paho.mqtt.client as mqtt 
+import time 
+
+broker = "10.96.185.112"  ## use ip address of wifi or use the ip address of esp 
+port = 1883 
+Topic = "AUR/controls"
+
+ 
+
+Current_Coordinate_X = 0.0
+Current_Coordinate_Y = 0.0
+
+def on_connect(client, userdata, flags, rc):
+
+    if rc == 0 :
+        print("Connected")
+        client.subscribe(Topic)
+        print(f"Connected to topic : {Topic} ")
+    else :
+        print("No Connectinon")  
+
+def on_message(client,userdata,msg):
+    message = msg.payload.decode()
+    print(f"Coordinates : {message}")         
+
+def Start_Subscriber():
+    client = mqtt.Client()
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+    
+    try :
+        client.connect(broker,port,60)
+    except :
+        print("An Error Has Occured")
+        return
+
+    client.loop_forever()        
+    
+    
+if __name__=="__main__":
+    Start_Subscriber()
